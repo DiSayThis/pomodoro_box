5ae7b7b3566e85e6dcbd627b536fe171b7957c4e
Dmitriy Izotov <disaythis@gmail.com>
2022-12-08 17:29:06 +0300

    add img & icons

--- src/shared/Components/Header/Header.tsx
+++ src/shared/Components/Header/Header.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './header.css';
 
 export function Header() {
-  return <header className={styles.header}></header>;
+  return <header className={styles.header}>pomodoro_box</header>;
 }
